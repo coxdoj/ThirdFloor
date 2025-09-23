a6b6d9db978754d189dfecef435969763d7c17d8
David Kwagala <kwagaladavid1@icloud.com>
2025-09-23 02:11:52 +0200

    Initial Commit

---
+++ ThirdFloor/ThirdFloorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThirdFloorApp.swift
+//  ThirdFloor
+//
+//  Created by David  on 23.09.25.
+//
+
+import SwiftUI
+
+@main
+struct ThirdFloorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
